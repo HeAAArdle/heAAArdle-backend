3f907a5d64cecc5c6b0e348846dfaf5c82d3743f
amabyado <amabyado@up.edu.ph>
2026-01-10 21:42:25 +0800

    chore: minor changes to game and song request / response schemas

--- app/schemas/game.py
+++ app/schemas/game.py
@@ -2,17 +2,24 @@ from pydantic import BaseModel
 from datetime import date
 from typing import Literal, List, Optional
 
+class StartGameRequest(BaseModel):
+    mode: Literal["original", "daily", "rapid", "lyrics"]
+    userID: Optional[str]
+
 class SubmitGameRequest(BaseModel):
     gameSessionID: str
     gameMode: Literal["original", "daily"]
     won: bool
     attempts: int
-    date: date | None
+    date: Optional[date]
 
 class GetArchivedDailyGameResultsResponse(BaseModel):
     numberOfDays: int
     startingDay: int
     results: List[Optional[bool]]
+
+    class Config:
+        from_attributes = True
 
 class UserWins(BaseModel):
     username: str
@@ -32,3 +39,6 @@ class GetLeaderboardDataDaily(BaseModel):
 class GetLeaderboardDataResponse(BaseModel):
     original: GetLeaderboardDataOriginal
     daily: GetLeaderboardDataDaily
+
+    class Config:
+        from_attributes = True
--- app/schemas/song.py
+++ app/schemas/song.py
@@ -1,42 +1,7 @@
 from pydantic import BaseModel
-from datetime import date
 
 class GetAllSongResponse(BaseModel):
     title: str
 
-class GetRandomSongResponse(BaseModel):
-    songID: str
-    title: str
-    artist: str
-    releaseYear: int
-    album: str | None
-    audio: str
-    link: str
-
-class GetDailySongResponse(BaseModel):
-    songID: str
-    title: str
-    artist: str
-    releaseYear: int
-    album: str | None
-    audio: str
-    link: str
-
-class GetArchivedDailySongResponse(BaseModel):
-    songID: str
-    title: str
-    artist: str
-    releaseYear: int
-    album: str | None
-    date: date
-    audio: str
-    link: str
-
-class GetLyricsModeSongResponse(BaseModel):
-    songID: str
-    title: str
-    artist: str
-    releaseYear: int
-    album: str | None
-    lyrics: str
-    link: str
+    class Config:
+        from_attributes = True
